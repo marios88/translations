a101129aeb3014f6fc82ef3b3640e382846c277c
marios88 <marios88@gmail.com>
2020-08-12 11:32:21 +0300

    Update Number.php

--- Language/el/Number.php
+++ Language/el/Number.php
@@ -22,9 +22,9 @@ return [
    'bytes'        => 'Bytes',
 
 	// don't forget the space in front of these!
-   'thousand'     => ' thousand',
-   'million'      => ' million',
-   'billion'      => ' billion',
-   'trillion'     => ' trillion',
-   'quadrillion'  => ' quadrillion',
+   'thousand'     => ' χίλια',
+   'million'      => ' εκατομύριο',
+   'billion'      => ' δισεκατομμύριο',
+   'trillion'     => ' τρισεκατομμύριο',
+   'quadrillion'  => ' τετρακισεκατομμύριον',
 ];
